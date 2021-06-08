84b70b9ae0673d1c799bfc3977b7ba09622dffff
Saentk <85130819+Saentk@users.noreply.github.com>
2021-06-08 13:14:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+const $arenas = document.querySelector('.arenas')
+const $randomButton = document.querySelector('.button')
 
-const Scorpion = {
+const player1 = {
+	player: 1,
 	name: 'Scorpion',
 	hp: 100,
 	img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
@@ -8,7 +11,8 @@ const Scorpion = {
 		console.log(Scorpion.name + ' Fight')
 	}
 }
-const Kitana = {
+const player2 = {
+	player: 2,
 	name : 'Kitana',
 	hp : 100,
 	img : 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
@@ -17,36 +21,58 @@ const Kitana = {
 		console.log(Kitana.name + ' Fight')
 	}
 }
-const Liukang = {
-	name : 'Liukang',
-	hp : 100,
-	img : 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
-	weapon : ['AK- 103', 'T64'],
-	attack : function() {
-		console.log(Liukang.name + ' Fight')
+
+function changeHP (playerType) {
+	const $playerLife = document.querySelector('.player' + playerType.player + ' .life');
+	playerType.hp -= Math.ceil(Math.random() * 20);
+	$playerLife.style.width = playerType.hp + '%';
+	console.log(player1.hp)
+
+	if (player1.hp < 0) {
+		$arenas.appendChild(playerWin(player2.name));
+		$randomButton.disabled = true
+	} else if (player2.hp < 0) {
+		$arenas.appendChild(playerWin(player1.name));
+		$randomButton.disabled = true
 	}
 }
 
-const $arenas = document.querySelector('.arenas')
 
+function playerWin(name) {
+	const $winTitle = addDiv('winTitle');
+	$winTitle.innerText = name + ' wins';
 
-// Создал функцию чтобы уменьшить количество повторений
+	return $winTitle
+}
+// alternative way to show winner (not active)
+function playerLose(name) {
+	const $loseTitle = addDiv('loseTitle');
+	$loseTitle.innerText = name + ' lose';
+
+	return $loseTitle
+}
+
+$randomButton.addEventListener('click', function(){
+	changeHP(player1);
+	changeHP(player2);
+})
+
+// Don`t want to repeat myself
 function addDiv(nameStr) {
 	$name = document.createElement('div')
 	$name.classList.add(nameStr)
 	return $name
 }
 
-function createPlayer(playerNum, playerType) {
+function createPlayer(playerType) {
 
-	$player = addDiv(playerNum)
+	$player = addDiv('player' + playerType.player)
 	$progressbar = addDiv('progressbar')
 	$progressbar = addDiv('progressbar')
 	$character = addDiv('character')
 	$life = addDiv('life')
 	$name = addDiv('name')
 
-	$arenas.appendChild($player)
 	$player.appendChild($progressbar)
 	$progressbar.appendChild($life)
 	$progressbar.appendChild($name)
@@ -62,13 +88,17 @@ function createPlayer(playerNum, playerType) {
 	return $player
 }
 
-createPlayer('player1', Scorpion)
-createPlayer('player2', Kitana)
+$arenas.appendChild(createPlayer(player1))
+$arenas.appendChild(createPlayer(player2))
 
 
 
 
 
+
+
+
+// You can choose other player
 // scorpion = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif'
 // kitana = 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif'
 // liukang = 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif'
